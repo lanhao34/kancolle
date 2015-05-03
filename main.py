fa397e3145d32a123c9fd013b98729157e5e856e
lanhao34 <lanhao34@gmail.com>
2015-05-03 21:02:44 +0800

    try to fix bug of repairing ss

--- main.py
+++ main.py
@@ -614,14 +614,14 @@ class AutoClick:
 
             if ship_id in ship_in_dock:
                 need_replace.add(i)
-            elif self.mission=='3-2':
-                if ship['api_sortno'] in self.stype[13] and ship['api_maxhp'] * 0.26 >= ship['api_nowhp']:
+            elif self.mission=='3-2' and ship['api_sortno'] in self.stype[13]:
+                if ship['api_maxhp'] * 0.26 >= ship['api_nowhp']:
                     need_repair.add(ship['index'])
                     need_replace.add(i)
             elif ship['api_maxhp'] * config['repair_hp_percent']>= ship['api_nowhp']:
                 need_repair.add(ship['index'])
                 need_replace.add(i)
-            if ship['api_cond'] < 33 and i in config['check_condition']:
+            elif ship['api_cond'] < 33 and i in config['check_condition']:
                 need_replace.add(i)
                 self.max_repair_time = max(
                     self.max_repair_time, now_time + (33 - ship['api_cond']) / 3 * 180 + 180)
